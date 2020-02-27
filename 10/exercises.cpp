0c9bc0d6d3b0cf4a7f318e767c6ff8cd73b54056
William E. Skeith <wes@cs.ccny.cuny.edu>
2020-02-27 16:19:07 -0500

    Notes and exercises.

---
+++ 10/exercises.cpp
@@ -0,0 +1,7 @@
+/* TODO: try to write down the exercises we outlined in class today.  In
+ * particular, the problem about expressing an integer as the sum of two
+ * entries in a vector, and the problem about making a vector unique (and
+ * doing so "in-place", and without taking a quadratic number of steps). */
+/* TODO: if you haven't done it already, try to write "binary search" for
+ * a sorted vector. */
+/* TODO: start on project 3!  Remember to do the warm-up first. */
